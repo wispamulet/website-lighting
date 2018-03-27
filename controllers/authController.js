c4bb8cd812be6d24ebda594869c412aae9d5c76f
WispAmulet <ylyzainali@gmail.com>
2018-03-27 14:59:18 +0800

    add send email

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const crypto = require('crypto');
 const promisify = require('es6-promisify');
 const mongoose = require('mongoose');
+const mail = require('../handlers/mail');
 
 const User = mongoose.model('User');
 
@@ -41,7 +42,15 @@ exports.forgot = async (req, res) => {
   await user.save();
   // 3. Send them an email with the token
   const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-  req.flash('success', `You have been emailed a password reset link. <a href="${resetURL}">Click -></a>`);
+
+  await mail.send({
+    user,
+    subject: 'Password Reset',
+    resetURL,
+    filename: 'password-reset'
+  });
+
+  req.flash('success', `You have been emailed a password reset link.`);
   // 4. Redirect to login page
   res.redirect('/login');
 };
