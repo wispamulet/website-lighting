854581caea92ba3cf3ac33ff8969b512b60280a2
WispAmulet <ylyzainali@gmail.com>
2018-07-03 16:48:59 +0800

    update 0703

--- public/javascripts/modules/formAddItem.js
+++ public/javascripts/modules/formAddItem.js
@@ -16,19 +16,19 @@ function addSpecification(element) {
     <button class="form__remove" onclick="this.parentElement.remove()"> &times; </button>
     <label for="model_no">Model No.</label>
     <input id="model_no" type="text" name="model_no">
-    <label for="power">Power</label>
+    <label for="power">Power (W)</label>
     <input id="power" type="text" name="power">
     <label for="base_type">Base Type</label>
     <input id="base_type" type="text" name="base_type">
-    <label for="lumen">Lumens</label>
+    <label for="lumen">Lumens (LM)</label>
     <input id="lumen" type="text" name="lumen">
-    <label for="dim">Dimensions</label>
+    <label for="dim">Dimensions (in)</label>
     <input id="dim" type="text" name="dim">
-    <label for="ctn_size">CTN Size</label>
+    <label for="ctn_size">CTN Size (in)</label>
     <input id="ctn_size" type="text" name="ctn_size">
-    <label for="ctn_quantity">CTN Quantity</label>
+    <label for="ctn_quantity">CTN Quantity (pcs)</label>
     <input id="ctn_quantity" type="text" name="ctn_quantity">
-    <label for="ctn_weight">CTN Weight</label>
+    <label for="ctn_weight">CTN Weight (lbs)</label>
     <input id="ctn_weight" type="text" name="ctn_weight">
   `;
 
