a404ffcb356cc02a7d430ec63a996d36ba4aef74
WispAmulet <ylyzainali@gmail.com>
2018-07-25 22:14:47 +0800

    add scroll smoothly

--- public/javascripts/modules/toTop.js
+++ public/javascripts/modules/toTop.js
@@ -1,5 +1,8 @@
 function rbToggle(topOfNav) {
-  if (document.body.scrollTop > topOfNav || document.documentElement.scrollTop > topOfNav) {
+  if (
+    document.body.scrollTop > topOfNav ||
+    document.documentElement.scrollTop > topOfNav
+  ) {
     document.body.classList.remove('rb--hide');
   } else {
     document.body.classList.add('rb--hide');
@@ -7,8 +10,9 @@ function rbToggle(topOfNav) {
 }
 
 function toTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  // document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
+  window.scroll({ top: 0, left: 0, behavior: 'smooth' }); // smooth scroll
 }
 
 export { rbToggle, toTop };
