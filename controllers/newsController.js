6a817f1844bdfd7abd646b981160ac843ea3298d
WispAmulet <ylyzainali@gmail.com>
2018-08-16 15:49:35 +0800

    update

--- controllers/newsController.js
+++
@@ -1,3 +0,0 @@
-exports.news = async (req, res) => {
-  res.render('news', { title: 'News' });
-};
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const navController = require('../controllers/navController');
 const productController = require('../controllers/productController');
 const projectController = require('../controllers/projectController');
-const newsController = require('../controllers/newsController');
 const certificateController = require('../controllers/certificateController');
-const userController = require('../controllers/userController');
-const authController = require('../controllers/authController');
+// const userController = require('../controllers/userController');
+// const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 const router = express.Router();
@@ -20,7 +19,7 @@ router.get('/about-us', navController.aboutUs);
 // products
 router.get(
   '/add-product',
-  authController.isLoggedIn,
+  // authController.isLoggedIn,
   productController.addProduct
 );
 router.post(
@@ -44,7 +43,7 @@ router.post(
 // projects
 router.get(
   '/add-project',
-  authController.isLoggedIn,
+  // authController.isLoggedIn,
   projectController.addProject
 );
 router.post(
@@ -64,15 +63,19 @@ router.post(
 );
 
 // news
-router.get('/news', catchErrors(newsController.news));
+// router.get('/news', catchErrors(newsController.news));
 
 // support
 router.get('/support/payment', navController.payment);
-router.get('/support/team', authController.isLoggedIn, navController.team);
+router.get(
+  '/support/team',
+  // authController.isLoggedIn,
+  navController.team
+);
 router.get('/support/certificates', certificateController.getCertificates);
 router.get(
   '/add-certificate',
-  authController.isLoggedIn,
+  // authController.isLoggedIn,
   certificateController.addCertificate
 );
 router.post(
@@ -103,24 +106,24 @@ router.post(
 );
 
 // user
-router.get('/register', userController.registerForm);
-router.post(
-  '/register',
-  userController.validateRegister, // 1. Validate the registration data
-  catchErrors(userController.register), // 2. Register the user
-  authController.login // 3. Log them in
-);
-router.get('/login', userController.loginForm);
-router.post('/login', authController.login);
-router.get('/logout', authController.logout);
-router.get('/account', userController.account);
-router.post('/account', catchErrors(userController.updateAccount));
-router.post('/account/forgot', catchErrors(authController.forgot));
-router.get('/account/reset/:token', catchErrors(authController.reset));
-router.post(
-  '/account/reset/:token',
-  authController.confirmPasswords,
-  catchErrors(authController.update)
-);
+// router.get('/register', userController.registerForm);
+// router.post(
+//   '/register',
+//   userController.validateRegister, // 1. Validate the registration data
+//   catchErrors(userController.register), // 2. Register the user
+//   authController.login // 3. Log them in
+// );
+// router.get('/login', userController.loginForm);
+// router.post('/login', authController.login);
+// router.get('/logout', authController.logout);
+// router.get('/account', userController.account);
+// router.post('/account', catchErrors(userController.updateAccount));
+// router.post('/account/forgot', catchErrors(authController.forgot));
+// router.get('/account/reset/:token', catchErrors(authController.reset));
+// router.post(
+//   '/account/reset/:token',
+//   authController.confirmPasswords,
+//   catchErrors(authController.update)
+// );
 
 module.exports = router;
